4123fceb9aedb74ee4356f5f1dff425216059abd
deepak-singh-19 <ds45063@gmail.com>
2025-08-27 15:30:55 +0530

    Intial commit

---
+++ main/practice.py
@@ -0,0 +1,10 @@
+# Practice print statement, comment, & character sequence
+
+print("hello Python")
+# print(8+5)
+# print(8-5)
+print(8*5)
+# print(8//5)
+print(8%5)
+print('deepak', 'Singh', 'Nayan')
+print("deepak", "Singh", "Dheer")
