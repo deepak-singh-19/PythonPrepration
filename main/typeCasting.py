be79404a045789d71ab0482c66736d5f81bac79c
deepak-singh-19 <ds45063@gmail.com>
2025-08-29 12:46:28 +0530

    User Input

--- main/typeCasting.py
+++ main/typeCasting.py
@@ -1,3 +1,19 @@
-a="20"
-b="10"
-print(int(a) + int(b))
+# a="20"
+# b="10"
+# # print(int(a) + int(b))
+#
+# c=12
+# d=15
+# print(int(a)*d)
+# print(a*d)
+# print(a+b)
+
+# str="10"
+# str1="20"
+# print(int(str) +int(str1))
+
+str = "18"
+number = 7
+str_number = int(str)
+sum = number + str_number
+print("The sum of the both number is:", sum)
---
+++ main/userInput.py
@@ -0,0 +1,12 @@
+# a = input("Enter a first number: ")
+# b = input("Enter a second number: ")
+#
+# print(int(a) + int(b))
+
+c = input("Enter a first number: ")
+d = input("Enter a second number: ")
+
+print(int(c) + int(d))
+print(int(c) - int(d))
+print(int(c) * int(d))
+print(int(c) / int(d))
