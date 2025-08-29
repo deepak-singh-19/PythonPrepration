5cdf24141932d19f258bebb37d759c7d60361dbb
deepak-singh-19 <ds45063@gmail.com>
2025-08-29 11:10:06 +0530

    update file

---
+++ main/operators.py
@@ -0,0 +1,10 @@
+a=10
+b=5
+
+# print(a+b) #-Addition
+# print(a-b) # Subtraction
+# print(a*b) # Multiplication
+# print(a/b) # Division Operator
+# print(a//b) # Double Divided
+# print(a%b) # Module
+# print(a**b) #Exponenational
