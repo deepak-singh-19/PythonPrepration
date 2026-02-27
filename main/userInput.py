80236f82fd4bfdc8c2a9bfa29f3c926db3567896
deepak-singh-19 <ds45063@gmail.com>
2026-02-27 17:02:43 +0530

    userinput

--- main/userInput.py
+++ main/userInput.py
@@ -1,8 +1,10 @@
+# Exercise 1
 # a = input("Enter a first number: ")
 # b = input("Enter a second number: ")
 #
 # print(int(a) + int(b))
 
+# Exercise 2
 # c = input("Enter a first number: ")
 # d = input("Enter a second number: ")
 #
@@ -11,10 +13,88 @@
 # print(int(c) * int(d))
 # print(int(c) / int(d))
 
-units = int(input("Enter total consumed electricity unit: "))
-per_unit_charge = 5
+# Exercise 3
+# units = int(input("Enter total consumed electricity unit: "))
+# per_unit_charge = 5
+#
+# if units <= 100:
+#     bill = units * per_unit_charge
+#
+#     print(f"Total Consumed Electricity : {bill}")
 
-if units <= 100:
-    bill = units * per_unit_charge
 
-    print(f"Total Consumed Electricity : {bill}")
+# Exercise 4
+
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# product_quantity = int(input("Enter your product quantity: "))
+# price = int(input("Enter your price: "))
+#
+# bill = price * product_quantity
+#
+# print(f"This invoice is system generated : {bill}")
+
+# Exercise 5
+
+# product_name = input("Enter your product name: ")
+# product_colour = input("Enter your product colour: ")
+# product_quantity = int(input("Enter your product quantity: "))
+# product_price = float(input("Enter your product price: "))
+#
+# price = product_quantity * product_price
+#
+# print(f"your price is: {price}")
+
+# Exercise 6 form fill up
+
+# your_name = input("Enter your name: ")
+# your_contactNumber = input("Enter your mobile number: ")
+# your_email = input("Enter your email: ")
+# your_experience = input("Enter your experience: ")
+# applyingFor_position = input("Enter your applying for: ")
+# ctc = input("Enter your ctc: ")
+# e_ctc = input("Enter your ectc: ")
+#
+# print("Thank you your job application is submitted")
+
+# Exercise 7 even or odd checker
+# number = int(input("Enter a number: "))
+# if number % 2 == 0:
+#     print(f"{number} is even")
+# else:
+#     print(f"{number} is odd")
+
+# Exercise 8 min & max value
+
+# a = 10
+# b = 5
+# c = 23
+# print(max(a,b,c))
+# print(min(a,b,c))
+
+# a = input("Enter a number: ")
+# b = input("Enter second number: ")
+# c = input("Enter third number: ")
+#
+# max = max(a,b,c)
+# #min = min(a,b,c)
+#
+# print(f"Your max number is : {max}")
+# # print(f"Your min number is : {min}")
+
+# Exercise 9 Area of Rectangle
+
+# length = float(input("Please enter the length of the side of the rectangle: "))
+# width = float(input("Please enter the width of the rectangle: "))
+# hight = float(input("Please enter the height of the rectangle: "))
+#
+# area = length * width * hight
+# print(f"The area of the rectangle is {area}cm")
+
+# Exercise 10 Area of the room
+
+length = float(input("Enter the length of the room: "))
+width = float(input("Enter the width of the room: "))
+
+room_area = length * width
+print(f"The area of the room is {room_area} SQ FT")
