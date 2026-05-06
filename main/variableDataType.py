870e3b6a8fad792a8b2e8137f5134a91d8168f14
deepak-singh-19 <ds45063@gmail.com>
2026-05-06 17:18:34 +0530

    exercise

--- main/variableDataType.py
+++ main/variableDataType.py
@@ -1,14 +1,50 @@
-a= 1
-b= 10.1
-c = "Deepak"
-d = None
-print("Type of a is", type(a))
-print("Type of a is", type(b))
-print("Type of a is", type(c))
-print("Type of a is", type(d))
+# a= 1
+# b= 10.1
+# c = "Deepak"
+# d = None
+# print("Type of a is", type(a))
+# print("Type of a is", type(b))
+# print("Type of a is", type(c))
+# print("Type of a is", type(d))
 
 # --list
 # -touple
 # --Map (dictionary)
 # dictionarydict ={"name":"deepak", "Age":25, "Mobile Number":5555555555,}
 # print(dict)
+
+#-- Variable exercise
+#---1 store personal information
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+# current_city = input("Enter your city: ")
+# native_place = input("Enter your native place: ")
+# company_name = input("Enter your company name: ")
+# job_role = input("Enter your job role: ")
+# print(name, age, current_city, native_place, company_name, job_role)
+
+#----Swap two number
+# a = 10
+# b = 20
+# a,b = b,a
+# print(a)
+# print(b)
+
+#---Calculate basic salary
+# basic_salary = int(input("Enter a basic salary: "))
+# bonus = int(input("Enter a bonus: "))
+# deduction = int(input("Enter a deduction: "))
+# total_salary = basic_salary + bonus - deduction
+# print("Your total salary is", total_salary)
+
+#--Product price calculater
+# product_name = input("Enter Your product name: ")
+# product_quantity = int(input("Enter Your product quantity: "))
+# product_price = int(input("Enter Your product price per item: "))
+# total_bill = product_quantity * product_price
+# print(total_bill)
+
+#-Login Credentials
+# name = input("Please enter your username: ")
+# password = int(input("Please enter your password: "))
+# print("Your username is " + name, "and password is ",password)
